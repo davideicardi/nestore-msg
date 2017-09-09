6e24cd5691a454f452d064ea70c2c8e98f3944c3
Davide Icardi <davide.icardi@gmail.com>
2017-09-09 17:27:26 +0200

    Added packages

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
+import {EventStore, ProjectionStream, Bucket, CommitData} from "nestore-js-mongodb";
+import * as MongoDb from "mongodb";
+import {EventEmitter} from "events";
+
 import * as Debug from "debug";
 const debug = Debug("nestore.msg");
 
